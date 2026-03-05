57cc26e03dbfe78e606ffa3a36b89682b49b8e91
riyyanqazi <riyyanqazii@gmail.com>
2026-03-05 13:04:14 +0500

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+  <img src="./assets/Screenshot/Screenshot 2026-03-05 122404.png" alt="Screenshot 1" width="300"/>
+
+<div style="display: flex; gap: 10px;">
+  <img src="./assets/Screenshot/Screenshot 2026-03-05 122439.png" alt="Screenshot 2" width="300"/>
+  <img src="./assets/Screenshot/Screenshot 2026-03-05 122459.png" alt="Screenshot 3" width="300"/>
+</div>
---
+++ app.js
@@ -0,0 +1,152 @@
+const htmlQuiz = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyper Tool Multi Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "Which HTML element is used to define the largest heading?",
+    options: ["<heading>", "<h6>", "<h1>", "<head>"],
+    answer: "<h1>",
+  },
+  {
+    question: "Which HTML element is used to create a hyperlink?",
+    options: ["<link>", "<a>", "<href>", "<url>"],
+    answer: "<a>",
+  },
+  {
+    question:
+      "Which attribute is used to provide an alternate text for an image?",
+    options: ["alt", "src", "title", "text"],
+    answer: "alt",
+  },
+  {
+    question: "Which HTML element is used to create a paragraph?",
+    options: ["<para>", "<p>", "<pg>", "<paragraph>"],
+    answer: "<p>",
+  },
+  {
+    question: "Which HTML element is used to create an unordered list?",
+    options: ["<ul>", "<ol>", "<li>", "<list>"],
+    answer: "<ul>",
+  },
+  {
+    question: "Which HTML element is used to define a table row?",
+    options: ["<td>", "<tr>", "<table>", "<row>"],
+    answer: "<tr>",
+  },
+  {
+    question: "Which HTML tag is used to display a picture on a webpage?",
+    options: ["<img>", "<image>", "<src>", "<picture>"],
+    answer: "<img>",
+  },
+  {
+    question: "Which HTML element defines the title of a document?",
+    options: ["<title>", "<head>", "<header>", "<h1>"],
+    answer: "<title>",
+  },
+  {
+    question: "Which HTML attribute specifies an inline style for an element?",
+    options: ["font", "style", "class", "styles"],
+    answer: "style",
+  },
+];
+let score = 0;
+let oneQuestion = 5;
+
+let userAnser = [];
+let Score = 0;
+let allAnswer = [];
+const correctAnswers = htmlQuiz.map((q) => q.answer);
+
+let progress_fill = document.querySelector(
+  ".progress-bar-custom .progress-fill",
+);
+
+let next_btn = document.querySelector(".next-btn");
+let question = document.querySelector("h4");
+let options = document.querySelectorAll(".answer-box .ans");
+
+let timer = document.querySelector(".timer");
+
+let count = 0;
+let questionsCount = 0;
+let question_count = document.querySelector(".quiz-card .question-count span");
+let time;
+
+if (window.location.href.includes("quizstart.html")) {
+  time = 8;
+
+  let c = setInterval(() => {
+    timer.innerText = `Timer : ${time}`;
+    time--;
+    if (time < 0) {
+      clearInterval(c);
+    }
+  }, 1000);
+}
+
+let num = 0;
+question.innerText = htmlQuiz[questionsCount].question;
+options.forEach((e, val) => {
+  options[val].addEventListener("click", (e) => {
+
+    options.forEach((opt) => (opt.style.border = "none"));
+    e.currentTarget.style.border = "1px solid #ffb300";
+    let useroption = options[val].innerText;
+    userAnser.push(useroption);
+    if (userAnser[val] === correctAnswers[val]) {
+      score += oneQuestion;
+
+    } 
+  });
+
+  // display on screen first time
+  options[val].innerText = htmlQuiz[questionsCount].options[val];
+  options[val].innerText = htmlQuiz[questionsCount].options[val];
+  options[val].innerText = htmlQuiz[questionsCount].options[val];
+  options[val].innerText = htmlQuiz[questionsCount].options[val];
+});
+let c;
+
+next_btn.addEventListener("click", (e) => {
+  clearInterval(c);
+
+  let a = 10;
+  count += a;
+  questionsCount++;
+
+  if (questionsCount >= htmlQuiz.length) {
+    let totalQuiz = 50;
+    localStorage.setItem("userScore", score);
+    localStorage.setItem("totalQuiz", totalQuiz);
+    window.location.href = "profile.html";
+    return;
+  }
+
+  time = 8;
+  timer.innerText = `Timer : ${time}`;
+  c = setInterval(() => {
+    time--;
+    timer.innerText = `Timer : ${time}`;
+    if (time <= 0) {
+      clearInterval(c);
+    }
+  }, 1000);
+
+  progress_fill.style.width = `${count}%`;
+  question_count.innerText = questionsCount + 1;
+  question.innerText = htmlQuiz[questionsCount].question;
+
+  options.forEach((opt, val) => {
+    opt.innerText = htmlQuiz[questionsCount].options[val];
+    opt.style.border = "none";
+  });
+});
+
+progress_fill.style.width = `${0}%`;
